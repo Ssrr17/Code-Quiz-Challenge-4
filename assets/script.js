7189595a3fcc9d806a27b7ec835a048b18273d8a
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-01-27 23:05:46 -0500

    html structured

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//Array to store quiz content 
+var multiChoice =[
+{ question: "placeholder question",
+  choices : ["a","b","c","d"],
+  answer : "b"
+},
+{ question: "placeholder question 2",
+  choices : ["a","b","c","d"],
+  answer : "d"
+}
+]
+
+
