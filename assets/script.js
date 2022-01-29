31c93cdeb296bdfd8423ae3838334f6b40890777
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-01-29 00:32:55 -0500

    setting timer

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,10 @@
 var questionEL = document.querySelector("#question")
 var choiceListEL = document.querySelector("#multichoice");
 var resultEl = document.querySelector("#result");
-var countDown = document.querySelector("#clock")
+var timerEl = document.querySelector("#time")
 var startBtnEl = document.querySelector("#startBtn")
+var countDown = 4;
+var clock;
 
 var questionNum = 0;
 var scoreCard = 0;
@@ -19,60 +21,96 @@ var multiChoice = [
         question: "placeholder question 2",
         choices: ["a", "b", "c", "d"],
         answer: "d"
+    },
+    {
+        question: "placeholder question 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "c"
     }
 ]
-
-
-var askQuestions = function () {
-    questionEL.textContent = multiChoice[questionNum].question;
-
-    choiceListEL.innerHTML = "";
-    resultEl.innerHTML = "";
-
-    var options = multiChoice[questionNum].choices;
-    var choiceArr = options.length;
-
-    for (var i = 0; i <= choiceArr; i++) {
-        var optionItem = document.createElement("li");
-        optionItem.textContent = options[i];
-        choiceListEL.append(optionItem)
-
-    }
-    
+// this will end game after last questions
+var gameOver = function () {
+    clearInterval(clock);
+    document.main.innerHTML = "GAME OVER! Your Score is :" + scoreCard;
 }
 
+//this will start the count down
+var tickTock = function () {
+    countDown--;
+    if (clock = 0) {
+        gameOver()
+    }
+}
+    
 
+    var askQuestions = function () {
 
-var verifyAns = function (event) {
-    //check if user selctions matches question answer
-    if (event.target.matches("li")) {
-        var pickedAns = event.target.textContent
-        if (pickedAns === multiChoice[questionNum].answer) {
-            resultEl.textContent = "You're Correct!"
-            scoreCard++;
-            console.log(scoreCard);
-        } else {
-            resultEl.textContent = "Wrong Answer!"
+        if (clock = 0) {
+            gameOver();
+            
         }
-    
+
+        clock = setInterval(tickTock, 1000);
+
+        questionEL.textContent = multiChoice[questionNum].question;
+
+        choiceListEL.innerHTML = "";
+        resultEl.innerHTML = "";
+
+
+        var options = multiChoice[questionNum].choices;
+        var choiceArr = options.length;
+
+        for (var i = 0; i <= choiceArr; i++) {
+            var optionItem = document.createElement("li");
+            optionItem.textContent = options[i];
+            choiceListEL.append(optionItem)
+
+        }
+
     }
-    //Add delay before next question is displayed
-    setTimeout(nexQues, 2000)
-}
-var nexQues = function(){
-    next = questionNum++;
-    askQuestions()
-}
+
+    var nexQues = function () {
+        questionNum++;
+        if (questionNum === multiChoice.length) {
+            clock = 0;
+            //return;
+
+        }
+        askQuestions()
+    }
 
 
 
-//document.querySelector("#next").addEventListener("click", function (){}
-choiceListEL.addEventListener("click",verifyAns)
+    var verifyAns = function (event) {
+        //Pause the clock
+        clearInterval(clock);
 
-var letsGetStarted = function(){
-    document.querySelector("#startBtn").addEventListener("click", askQuestions)
-}
-letsGetStarted()
+        //check if user selctions matches question answer
+        if (event.target.matches("li")) {
+            var pickedAns = event.target.textContent
+            if (pickedAns === multiChoice[questionNum].answer) {
+                resultEl.textContent = "You're Correct!"
+                scoreCard++;
+                console.log(scoreCard);
+            } else {
+                resultEl.textContent = "Wrong Answer!"
+                countDown = countDown - 2;
+            }
+
+        }
+        //Add delay before next question is displayed
+        setTimeout(nexQues, 2000)
+    }
+  
+
+    //document.querySelector("#next").addEventListener("click", function (){}
+    choiceListEL.addEventListener("click", verifyAns)
+
+    var letsGetStarted = function () {
+        document.querySelector("#startBtn").addEventListener("click", askQuestions)
+    }
+    letsGetStarted()
 //askQuestions()
 
 
