39c3badc9c96e7ca3ea3cb7b5a368f218d920677
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-01-30 22:05:24 -0500

    intialandscore

--- assets/script.js
+++ assets/script.js
@@ -4,113 +4,184 @@ var choiceListEL = document.querySelector("#multichoice");
 var resultEl = document.querySelector("#result");
 var timerEl = document.querySelector("#time")
 var startBtnEl = document.querySelector("#startBtn")
-var countDown = 4;
+var countDown = 30;
 var clock;
-
+var initialEl = document.querySelector("#initial")
 var questionNum = 0;
 var scoreCard = 0;
 
 
 var multiChoice = [
     {
-        question: "placeholder question 1",
-        choices: ["a", "b", "c", "d"],
-        answer: "b"
+        question: "How many players begin a soccer game for each team?",
+        choices: ["7", "14", "17", "11"],
+        answer: "11"
     },
     {
-        question: "placeholder question 2",
-        choices: ["a", "b", "c", "d"],
-        answer: "d"
+        question: "Which one of the following is allowed to use their hands in the game",
+        choices: ["Defender", "Winger", "Goalkeeper", "Foward"],
+        answer: "Goalkeeper"
     },
     {
-        question: "placeholder question 3",
-        choices: ["a", "b", "c", "d"],
-        answer: "c"
+        question: "How many referees are on the pitch during a game",
+        choices: ["3", "4", "0", "1"],
+        answer: "1"
+    },
+    {
+        question: "How many minutes are there in one half of a soccer game",
+        choices: ["12 minutes", "25 minutes", "45 minutes", "90 minutes"],
+        answer: "45 minutes"
+    },
+    {
+        question: "Which one of the following can score during the game",
+        choices: ["Midfielders", "Defenders", "Fowards", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "How long is a soccer game",
+        choices: ["90 minutes", "180 minutes", "45 minutes", "1 hour"],
+        answer: "90 minutes"
+    },
+    {
+        question: "How many points is one goal on the scoreboard",
+        choices: ["7", "2", "1", "3"],
+        answer: "1"
+    },
+    {
+        question: "What happens when a player gets a red card?",
+        choices: ["Player gets a warning", "Player gets a reward", "Player has to leave the game", "Player gets substituted"],
+        answer: "Player has to leave the game"
+    },
+    {
+        question: "What is the number of substitutions allowed for each team",
+        choices: ["3", "1", "5", "11"],
+        answer: "3"
+    },
+    {
+        question: "What postiosn is also called 'the number 9' position",
+        choices: ["Center-back", "Striker", "Left-back", "Attacking-midfielder"],
+        answer: "Striker"
     }
+
 ]
+getIntial = function () {
+    var userIn = prompt("Please enter initials")
+    if (userIn != "" && userIn != null) {
+        document.getElementById("demo").innerHTML =
+        alert(" Player " + userIn + " Score " + scoreCard);
+        
+    } else {
+        alert("Please enter your initials");
+        return getIntial()
+    }
+    localStorage.setItem(userIn,scoreCard);
+}
+
+
+
+
+
 // this will end game after last questions
 var gameOver = function () {
     clearInterval(clock);
-    document.main.innerHTML = "GAME OVER! Your Score is :" + scoreCard;
+    resultEl.textContent = "GAME OVER!";
+    getIntial()
+    
+   return;
+   
 }
 
 //this will start the count down
 var tickTock = function () {
     countDown--;
-    if (clock = 0) {
+    if (countDown <= 0) {
         gameOver()
     }
+    timerEl.textContent = "Time  : " + countDown;
+    timerEl.className = "stlyetime";
+
 }
-    
-
-    var askQuestions = function () {
-
-        if (clock = 0) {
-            gameOver();
-            
-        }
-
-        clock = setInterval(tickTock, 1000);
-
-        questionEL.textContent = multiChoice[questionNum].question;
-
-        choiceListEL.innerHTML = "";
-        resultEl.innerHTML = "";
 
 
-        var options = multiChoice[questionNum].choices;
-        var choiceArr = options.length;
+var askQuestions = function () {
 
-        for (var i = 0; i <= choiceArr; i++) {
-            var optionItem = document.createElement("li");
-            optionItem.textContent = options[i];
-            choiceListEL.append(optionItem)
 
-        }
+    timerEl.textContent = countDown;
+
+    if (clock = 0) {
+        gameOver();
 
     }
 
-    var nexQues = function () {
-        questionNum++;
-        if (questionNum === multiChoice.length) {
-            clock = 0;
-            //return;
+    clock = setInterval(tickTock, 1000);
 
+    questionEL.textContent = multiChoice[questionNum].question;
+    questionEL.className = "qcard";
+
+    choiceListEL.innerHTML = "";
+    resultEl.innerHTML = "";
+
+
+    var options = multiChoice[questionNum].choices;
+    var choiceArr = options.length;
+
+    for (var i = 0; i < choiceArr; i++) {
+        var optionItem = document.createElement("li");
+        optionItem.className = "anscard";
+        optionItem.textContent = options[i];
+        choiceListEL.append(optionItem)
+
+    }
+
+}
+
+var nexQues = function () {
+    questionNum++;
+    if (questionNum === multiChoice.length) {
+        clock = 0;
+        gameOver();
+        //console.log(scoreCard);
+        return;
+
+    }
+    askQuestions()
+}
+
+
+
+var verifyAns = function (event) {
+    //Pause the clock
+    clearInterval(clock);
+
+    //check if user selctions matches question answer
+    if (event.target.matches("li")) {
+        var pickedAns = event.target.textContent
+        if (pickedAns === multiChoice[questionNum].answer) {
+            resultEl.textContent = "You're Correct!"
+            scoreCard++;
+            console.log(scoreCard);
+        } else {
+            resultEl.textContent = "Wrong Answer!"
+            countDown = countDown - 2;
+            console.log(countDown);
         }
-        askQuestions()
+
     }
+    //Add delay before next question is displayed
+    setTimeout(nexQues, 2000)
+}
 
 
+//document.querySelector("#next").addEventListener("click", function (){}
+choiceListEL.addEventListener("click", verifyAns)
 
-    var verifyAns = function (event) {
-        //Pause the clock
-        clearInterval(clock);
+var letsGetStarted = function () {
+    document.querySelector("#startBtn").addEventListener("click", askQuestions)
 
-        //check if user selctions matches question answer
-        if (event.target.matches("li")) {
-            var pickedAns = event.target.textContent
-            if (pickedAns === multiChoice[questionNum].answer) {
-                resultEl.textContent = "You're Correct!"
-                scoreCard++;
-                console.log(scoreCard);
-            } else {
-                resultEl.textContent = "Wrong Answer!"
-                countDown = countDown - 2;
-            }
+}
 
-        }
-        //Add delay before next question is displayed
-        setTimeout(nexQues, 2000)
-    }
-  
 
-    //document.querySelector("#next").addEventListener("click", function (){}
-    choiceListEL.addEventListener("click", verifyAns)
-
-    var letsGetStarted = function () {
-        document.querySelector("#startBtn").addEventListener("click", askQuestions)
-    }
-    letsGetStarted()
+letsGetStarted()
 //askQuestions()
 
 
